39de8dd871c1ecded2b05e1c282cd71214bb252c
lyduybach2612 <lyduybach900@gmail.com>
2026-03-29 23:19:08 +0700

    feat: add base code and login logout for frontend

---
+++ frontend/src/components/logo/LogoIcon.tsx
@@ -0,0 +1,7 @@
+import logoIcon from 'assets/images/tlu/tlu-badge.png';
+
+// ==============================|| LOGO ICON SVG ||============================== //
+
+export default function LogoIcon() {
+  return <img src={logoIcon} alt="TLU" width="35" />;
+}
