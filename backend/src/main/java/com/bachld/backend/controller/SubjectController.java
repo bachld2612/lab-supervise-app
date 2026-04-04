a67125e002b534627453adbfe130c9a4da8fa342
lyduybach2612 <lyduybach900@gmail.com>
2026-04-04 21:30:15 +0700

    feat: build ui and fix api for subject page

--- backend/src/main/java/com/bachld/backend/controller/SubjectController.java
+++ backend/src/main/java/com/bachld/backend/controller/SubjectController.java
@@ -52,4 +52,11 @@ public class SubjectController {
         subjectService.update(request, id);
         return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK.value(), null));
     }
+
+    @DeleteMapping("/v1/{id}")
+    @AuthFilter(role = "ADMIN")
+    public ResponseEntity<?> deleteById(@PathVariable int id) {
+        subjectService.deleteById(id);
+        return ResponseEntity.ok(new BaseResponse<>(HttpStatus.OK.value(), null));
+    }
 }
